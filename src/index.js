b9a7124c1fbd297df9ea97bacad92d5b1da98684
geralldtit <geralldtit@gmail.com>
2020-03-13 13:21:48 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  if(expr.length%10!=0) return "Error: Input string is not a multiple of 10!";
+
+  var result = [];
+  for(let i = 0; i < expr.length; i+=10){
+    result.push(convertBinaryToText(expr.substr(i, 10)));
+  }
+
+  return result.join('');
+}
+
+function convertBinaryToText(encodedChar){
+  if(encodedChar[0] == "*") return " ";
+  let morse = [];
+  for(let k = 0; k < encodedChar.length; k+=2){
+    if( !(encodedChar[k] == encodedChar[k+1] && encodedChar[k] == "0") ){
+      morse.push((encodedChar[k]+encodedChar[k+1]) == "10" ? "." : "-");
+    }
+  }
+  return MORSE_TABLE[morse.join('')];
 }
 
 module.exports = {
